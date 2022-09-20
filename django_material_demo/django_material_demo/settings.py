1d2e81f94a10f8beb50e2c61a9ed75b02a60caad
Rubikium <30288416+Rubikium@users.noreply.github.com>
2022-09-20 11:57:50 +0800

    Add logging settings

--- django_material_demo/django_material_demo/settings.py
+++ django_material_demo/django_material_demo/settings.py
@@ -82,6 +82,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_material_demo.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
