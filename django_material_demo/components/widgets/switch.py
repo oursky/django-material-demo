cd71d9ccbbc92bab82d60cc768c35828efce22b1
Tung Wu <tung@oursky.com>
2022-09-23 18:03:37 +0800

    Add comments in switch widget

--- django_material_demo/components/widgets/switch.py
+++ django_material_demo/components/widgets/switch.py
@@ -1,5 +1,7 @@
 from django.forms import Widget
 
 class Switch(Widget):
+    # django-material will automatically find the template in "material/fields/{module}_{class}.html",
+    # so the following line is just for reference
     # template_name = "material/fields/component_switch.html"
     pass
