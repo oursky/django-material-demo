956bdb5fe7d7417ad29990be8382cf02ab60d8cb
Rubikium <30288416+Rubikium@users.noreply.github.com>
2022-10-13 20:04:43 +0800

    Move library patches into separate files

--- django_material_demo/django_material_demo/settings.py
+++ django_material_demo/django_material_demo/settings.py
@@ -163,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = 'media/'
