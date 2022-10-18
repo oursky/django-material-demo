bc7a1441bd53d2848b46c31b5c4e517976fbc9f5
Rubikium <30288416+Rubikium@users.noreply.github.com>
2022-10-18 16:00:33 +0800

    Change choices to not be soft-deleted

--- django_material_demo/polls/models.py
+++ django_material_demo/polls/models.py
@@ -121,7 +121,7 @@ class Question(SafeDeleteModel):
         return list(choices.values_list('choice_text', flat=True))
 
 
-class Choice(SafeDeleteModel):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     vote_count = models.IntegerField(default=0)
